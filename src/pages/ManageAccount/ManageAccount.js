03eae58ad28221a3edb121459eefb7495f046c85
lab6TranTrungKien <19130110@st.hcmuaf.edu.vn>
2023-03-29 21:00:31 +0700

    update-info-user++

---
+++ src/pages/ManageAccount/ManageAccount.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+import SideBar from './components/Sidebar';
+import sidebar_menu from './constants/sidebar-menu';
+
+
+import MainContent from "./pages/MainContent";
+import classNames from "classnames/bind";
+import styles from "./ManageAccount.module.scss";
+const cx = classNames.bind(styles);
+function ManageAccount() {
+    return (
+
+        <div className={cx('dashboard-container')}>
+            <SideBar menu={sidebar_menu}/>
+            <MainContent/>
+
+        </div>
+
+    )
+}
+
+export default ManageAccount;
+
+
